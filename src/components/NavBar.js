334217849fe8d8fc462e60757f5b28e2d669066a
obify <76449271+obify@users.noreply.github.com>
2023-04-08 18:15:41 +0200

    Update NavBar.js
    added new changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,7 +16,7 @@ function NavBar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand text-uppercase" to="/top5Sales">Sales App</NavLink>
+                    <NavLink className="navbar-brand text-uppercase" to="/top5Sales">Sales App V2</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
